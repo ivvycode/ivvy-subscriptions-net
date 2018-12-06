efb3def214db5201ab9a70cbe836e691c89b17f9
David Tannock <david.tannock@ivvy.com>
2018-12-06 17:14:39 +1000

    More information has been added to the "BookingDeleted" message.

--- Messages/Venues/Bookings/BookingDeleted.cs
+++ Messages/Venues/Bookings/BookingDeleted.cs
@@ -21,5 +21,23 @@ namespace Ivvy.Subscriptions.Messages.Venues.Bookings
         /// </summary>
         [JsonProperty("bookingId")]
         public int BookingId { get; set; }
+
+        /// <summary>
+        /// The status of the booking when it was deleted.
+        /// </summary>
+        [JsonProperty("bookingStatus")]
+        public Booking.StatusOptions BookingStatus { get; set; }
+
+        /// <summary>
+        /// The unique ids of the group accommodation that belonged to the deleted booking.
+        /// </summary>
+        [JsonProperty("accommodationIds")]
+        public int[] AccommodationIds { get; set; }
+
+        /// <summary>
+        /// The unique ids of the room reservations that belonged to the deleted booking.
+        /// </summary>
+        [JsonProperty("roomReservationIds")]
+        public int[] RoomReservationIds { get; set; }
     }
 }
