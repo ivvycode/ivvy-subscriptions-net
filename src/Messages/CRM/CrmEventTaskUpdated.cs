f1b4423a78319d527b625b10124bc13d2ad62fa4
Haaris Ahmad <haaris.ahmad@ivvy.com>
2021-05-27 17:44:15 +1000

    AE-1941: added event task subscriptions

---
+++ src/Messages/CRM/CrmEventTaskUpdated.cs
@@ -0,0 +1,23 @@
+﻿using Ivvy.API.Crm;
+using Newtonsoft.Json;
+
+namespace Ivvy.Subscriptions.Messages.CRM
+{
+    /// <summary>
+    /// A task was updated.
+    /// </summary>
+    public class CrmEventTaskUpdated
+    {
+        /// <summary>
+        /// The details of the updated task.
+        /// </summary>
+        [JsonProperty("data")]
+        public EventTask CrmEventTask;
+
+        /// <summary>
+        /// Any previous information of the task that was changed.
+        /// </summary>
+        [JsonProperty("previousData")]
+        public dynamic PreviousData;
+    }
+}
